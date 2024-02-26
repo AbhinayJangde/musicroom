4cbeaa1c008a48bce898267641565fca06802c5e
Abhinay Jangde <abhinayjangde@gmail.com>
2024-02-26 20:34:11 +0530

    building featured card section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,10 +11,10 @@ const HeroSection = () => {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className='p-4 relative z-10 w-full text-center'>
+      <div className='p-4 relative z-10 w-full  text-center'>
         <h1 className='mt-10 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Master the art of music</h1>
         <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>{`Dive into our comprehensive music courses and transform your musical journey today. Wheater you're beginner or looking to refine your skill, join us to unlock your true potential.`}</p>
-        <div className="mt-4">
+        <div className="mt-20">
           <Link href={'/courses'}>
             <Button
               borderRadius="1.75rem"
